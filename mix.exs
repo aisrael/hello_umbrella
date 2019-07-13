05ca21b651d3a549eec1df7b2de14e2d7fa32660
Alistair A. Israel <aisrael@gmail.com>
2019-07-13 11:07:58 +0800

    Dockerised (use make to build)

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Hello.Umbrella.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [
-        prod: [
+        hello: [
           applications: [hello: :permanent, hello_web: :permanent]
         ]
       ]
